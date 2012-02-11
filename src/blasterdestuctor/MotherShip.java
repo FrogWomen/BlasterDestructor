5f2e570bbffc2329249679dbd554aa791cfc560d
jennifer <jennifer@10.0.1.5>
2012-02-11 12:25:50 -0800

    did stuff.

--- src/blasterdestuctor/MotherShip.java
+++ src/blasterdestuctor/MotherShip.java
@@ -1,7 +1,9 @@
 package blasterdestuctor;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.AffineTransform;
 
 public class MotherShip
 {
@@ -10,30 +12,32 @@ public class MotherShip
     private int froggyCourse;
     private int froggySpeed;
     private Image froggyImage;
-
+    private AffineTransform motherShipAT;
+    
     public void paintSelf(Graphics2D g2)
     {
-g2.translate(-155, -155);
+        g2.translate(-155, -155);
         g2.rotate(Math.toRadians(froggyCourse));
         g2.drawImage(froggyImage, 0, 0, null);
-        
+        g2.setColor(Color.green);
+        int froggyWidth = froggyImage.getWidth(null);
+        g2.drawRect(0, 0, froggyImage.getWidth(null), froggyImage.getHeight(null));
     }
-
+    
     public void setFroggyCourse(int froggyCourseJenny)
     {
         froggyCourse = froggyCourseJenny;
         System.out.println(froggyCourse);
     }
-
+    
     public void setFroggySpeed(int froggySpeedJenny)
     {
         froggySpeed = froggySpeedJenny;
         System.out.println(froggySpeed);
     }
-
+    
     public void setFroggyImage(Image froggyImage)
     {
         this.froggyImage = froggyImage;
     }
 }
-
