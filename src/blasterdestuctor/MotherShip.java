35791e351fcc107f65a1ed9e67f5809383e1544b
jennifer <jennifer@10.0.1.5>
2012-02-18 12:47:17 -0800

    MAde the froggy move.

--- src/blasterdestuctor/MotherShip.java
+++ src/blasterdestuctor/MotherShip.java
@@ -4,6 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
 public class MotherShip
 {
@@ -13,29 +18,53 @@ public class MotherShip
     private int froggySpeed;
     private Image froggyImage;
     private AffineTransform motherShipAT;
-    
+    private Area motherShipArea;
+    private Rectangle2D.Double motherShipRectangle;
+    private URL froggyImageAdress;
+    private int screenX = 500;
+    private int screenY = 500;
+
+    public MotherShip()
+    {
+        froggyImageAdress = getClass().getResource("TreeFrogMotherShip.png");
+        try
+        {
+            froggyImage = ImageIO.read(froggyImageAdress);
+        } catch (IOException ex)
+        {
+            System.out.println("hiccup trying to read froggyImage");
+        }
+        int froggyWidth = froggyImage.getWidth(null);
+        int froggyHeight = froggyImage.getHeight(null);
+        motherShipRectangle = new Rectangle2D.Double(0, 0, froggyWidth, froggyHeight);
+        motherShipArea = new Area(motherShipRectangle);
+    }
+
     public void paintSelf(Graphics2D g2)
     {
-        g2.translate(-155, -155);
+        froggyDeltaX = (int) (froggySpeed * Math.sin(Math.toRadians(froggyCourse)));
+        froggyDeltaY = -(int) (froggySpeed * Math.cos(Math.toRadians(froggyCourse)));
+        screenX += froggyDeltaX;
+        screenY += froggyDeltaY;
+        g2.translate(screenX, screenY);
         g2.rotate(Math.toRadians(froggyCourse));
         g2.drawImage(froggyImage, 0, 0, null);
         g2.setColor(Color.green);
-        int froggyWidth = froggyImage.getWidth(null);
-        g2.drawRect(0, 0, froggyImage.getWidth(null), froggyImage.getHeight(null));
+        g2.draw(motherShipArea);
     }
-    
+
     public void setFroggyCourse(int froggyCourseJenny)
     {
         froggyCourse = froggyCourseJenny;
         System.out.println(froggyCourse);
     }
-    
+
     public void setFroggySpeed(int froggySpeedJenny)
     {
         froggySpeed = froggySpeedJenny;
         System.out.println(froggySpeed);
     }
-    
+
     public void setFroggyImage(Image froggyImage)
     {
         this.froggyImage = froggyImage;
